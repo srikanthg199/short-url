383728802884d7b1a1250727336335f7c3d81ade
srikanthg199 <srikanthdevarakonds@gmail.com>
2025-02-18 15:45:37 +0530

    Local db changes

--- config/config.js
+++ config/config.js
@@ -4,15 +4,23 @@ const path = require('path');
 
 module.exports = {
     development: {
-        url: process.env.DB_URL,  // Ensure this is correctly set
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                ca: fs.readFileSync(path.resolve(__dirname, '../ca.pem')),
-                require: true,
-                rejectUnauthorized: true
-            }
-        }
-    }
+        username: process.env.DB_USERNAME || 'default_username',
+        password: process.env.DB_PASSWORD || 'default_password',
+        database: process.env.DB_NAME || 'default_dbname',
+        host: process.env.DB_HOST || '127.0.0.1',
+        dialect: process.env.DB_DIALECT || 'postgres',
+        port: process.env.DB_PORT || 5432,
+    },
+    // development: {
+    //     url: process.env.DB_URL,  // Ensure this is correctly set
+    //     dialect: 'postgres',
+    //     dialectOptions: {
+    //         ssl: {
+    //             ca: fs.readFileSync(path.resolve(__dirname, '../ca.pem')),
+    //             require: true,
+    //             rejectUnauthorized: true
+    //         }
+    //     }
+    // }
 };
 
--- models/index.js
+++ models/index.js
@@ -1,23 +1,33 @@
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require("sequelize");
-require('dotenv').config();
+const dbConfig = require("../config/config").development
 
-// Read the certificate file
-const sslCert = fs.readFileSync(path.resolve(__dirname, '../ca.pem'));
-const dbUrl = process.env.DB_URL
+// // Read the certificate file
+// const sslCert = fs.readFileSync(path.resolve(__dirname, '../ca.pem'));
+// const dbUrl = process.env.DB_URL
 
-const sequelize = new Sequelize(dbUrl, {
-  dialectOptions: {
-    ssl: {
-      ca: sslCert, // Pass the certificate to the connection
-      require: true, // Ensure SSL is used
-      rejectUnauthorized: true // Enforce certificate validation
-    }
+// const sequelize = new Sequelize(dbUrl, {
+//   dialectOptions: {
+//     ssl: {
+//       ca: sslCert, // Pass the certificate to the connection
+//       require: true, // Ensure SSL is used
+//       rejectUnauthorized: true // Enforce certificate validation
+//     }
+//   }
+// });
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    port: dbConfig.port,
+    dialect: dbConfig.dialect,
+    logging: false,
   }
-});
-
-
+);
 
 const db = {
   Sequelize,
